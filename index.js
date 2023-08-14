41e56b5166565edf97da17df1f3b3294cacb8216
Anthony26888 <haidang11111995@gmail.com>
2023-08-14 16:03:05 +0700

    update code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var perBNB = document.getElementById("PerBNB")
 
 
 
-const apiUrl = `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cbinancecoin&vs_currencies=usd&include_24hr_change=true&include_last_updated_at=true&precision=2`;
+const apiUrl = `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cbinancecoin%2Cripple%2Cdogecoin%2Cbitget-token&vs_currencies=usd&include_24hr_change=true&include_last_updated_at=true&precision=2`;
 
 
 function showPrice(){
@@ -40,20 +40,3 @@ showPrice();
 
 
 
-
-
-let items = document.querySelectorAll('.carousel .carousel-item')
-
-		items.forEach((el) => {
-			const minPerSlide = 3
-			let next = el.nextElementSibling
-			for (var i=1; i<minPerSlide; i++) {
-				if (!next) {
-            // wrap carousel by using first child
-            next = items[0]
-        }
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
-})
