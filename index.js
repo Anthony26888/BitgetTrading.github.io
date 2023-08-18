13f6b558b871ed06c9955f54797e9938a8464f90
Anthony26888 <haidang11111995@gmail.com>
2023-08-18 15:43:01 +0700

    update code

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 
 
 
-
 function showPriceTop(){
   var btcPrice = document.getElementById("btcPrice")
   var ethPrice = document.getElementById("ethPrice")
@@ -63,16 +62,16 @@ function showPriceTop(){
 }
 showPriceTop();
 
-const rankTrending = document.getElementById("rank-table-trending");
+
 function showTrending(){
-  
+  const rankTrending = document.getElementById("rank-table-trending");
   const apiUrl = `https://api.coingecko.com/api/v3/search/trending`;
   fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
     console.log(data)
     const trendingCoin = data.coins
-    trendingCoin.forEach(value => {
+    trendingCoin.slice(0, 4).forEach(value => {
       
       const imgCoin = value.item.small;
       const nameCoin = value.item.symbol;
@@ -97,3 +96,72 @@ function showTrending(){
   });
 }
 showTrending();
+
+function showGainer(){
+  const rankGainer = document.getElementById("rank-table-gainer");
+  const apiUrl = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=volume_desc&per_page=250&page=1&sparkline=false&price_change_percentage=24h&locale=en`;
+  fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    
+    const valueSort = data.sort((a,b) => b.price_change_percentage_24h - a.price_change_percentage_24h);
+    valueSort.slice(0, 4).forEach(value =>{
+      console.log(value)
+      const imgCoin = value.image;
+      const nameCoin = value.symbol;
+      const priceCoin = value.current_price
+      const maketcap = value.price_change_percentage_24h;
+      let newTd = document.createElement("tr");      
+      newTd.innerHTML=`        
+        <td style="background-color:transparent; color: #fff; font-size:14px; ">
+          <img src="${imgCoin}" class="icon-coin-rank" alt="SEI">
+          <span style="text-transform: uppercase;">${nameCoin}/USDT</span>
+        </td>
+        <td style="background-color:transparent; color: #1DA2B4; font-size:14px; text-align: center;">${priceCoin.toFixed(8)}</td>
+        <td style="background-color:transparent; color: #1DA2B4; text-align: center;">${maketcap.toFixed(2)}%</td>      
+      `
+      rankGainer.appendChild(newTd);
+    })
+  })
+}
+showGainer();
+
+function showNewCoin(){
+  const rankNew = document.getElementById("rank-table-new");
+  const newCoinRank = [
+    {
+    "coin1":"fasttoken",
+    "coin2":"cyberconnect",
+    "coin3":"sei-network",
+    "coin4":"bad-idea-ai",
+    "coin5":"harrypotterobamapacman8inu",
+    }
+  ]
+  newCoinRank.forEach(value =>{
+    const apiUrl = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${value[0]}%2Cethereum%2Cbinancecoin%2Clitecoin&page=1&sparkline=false&price_change_percentage=24h&locale=en&precision=8`;
+    console.log(value.coin1)
+  })
+  
+  fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(value =>{
+      console.log(value)
+      const imgCoin = value.image;
+      const nameCoin = value.symbol;
+      const priceCoin = value.current_price
+      const percentCoin = value.price_change_percentage_24h;
+      let newTd = document.createElement("tr");      
+      newTd.innerHTML=`        
+        <td style="background-color:transparent; color: #fff; font-size:14px; ">
+          <img src="${imgCoin}" class="icon-coin-rank" alt="SEI">
+          <span style="text-transform: uppercase;">${nameCoin}/USDT</span>
+        </td>
+        <td style="background-color:transparent; color: #1DA2B4; font-size:14px; text-align: center;">${priceCoin.toFixed(8)}</td>
+        <td style="background-color:transparent; color: #1DA2B4; text-align: center;">${percentCoin.toFixed(2)}%</td>      
+      `
+      rankNew.appendChild(newTd);
+    })
+  })
+}
+showNewCoin();
