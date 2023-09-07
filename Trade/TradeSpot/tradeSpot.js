5e6ca629a888abff22da7bdcfcb7270274011fa3
Anthony26888 <haidang11111995@gmail.com>
2023-09-07 16:03:36 +0700

    update code

--- Trade/TradeSpot/tradeSpot.js
+++ Trade/TradeSpot/tradeSpot.js
@@ -57,8 +57,8 @@ function showOderBook(){
             const total = priceOrder*amountOrder;
             newTr = document.createElement("tr")
             newTr.innerHTML=`
-                <td style="color:rgb(241 73 63)">${priceOrder}</td>
-                <td>${amountOrder}</td>
+                <td style="color:rgb(241 73 63)">${Number(priceOrder).toFixed(2)}</td>
+                <td>${Number(amountOrder).toFixed(6)}</td>
                 <td>${total.toFixed(4)}</td>
             `
             bidBook.appendChild(newTr)
@@ -80,8 +80,8 @@ function showOderBook(){
             const total = priceOrder*amountOrder;
             newTr = document.createElement("tr")
             newTr.innerHTML=`
-                <td style="color:rgb(29 162 180)">${priceOrder}</td>
-                <td>${amountOrder}</td>
+                <td style="color:rgb(29 162 180)">${Number(priceOrder).toFixed(2)}</td>
+                <td>${Number(amountOrder).toFixed(6)}</td>
                 <td>${total.toFixed(4)}</td>
             `
             askBook.appendChild(newTr)
@@ -108,6 +108,7 @@ showOderBook()
 
 function showPriceCoin(){
     const priceCoin = document.getElementById("infoCoin")
+    const infoCoin = document.getElementById("infoCoin2")
     priceCoin.innerHTML="";
     const apiUrl =`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&page=1&sparkline=false&price_change_percentage=24h&locale=en&precision=2`
     fetch(apiUrl)
@@ -123,6 +124,14 @@ function showPriceCoin(){
             const low24h = value.low_24h;
             const volume = value.total_volume;
             const turnover = value.market_cap_change_24h;
+
+            infoCoin.innerHTML=`
+                <img class="mt-3" src="${image}" alt="" style="width: 30px; height:30px">
+                <div class="d-flex flex-sm-column p-1">
+                    <h5 class="text-light text-uppercase">${symbol}/USDT</h5>
+                    <small class="text-capitalize"><i class="fa fa-info-circle" aria-hidden="true"></i>${name}</small>
+                </div>
+            `
             const newDiv = document.createElement("div")
             newDiv.classList.add("d-flex")
             newDiv.innerHTML=`
@@ -152,14 +161,44 @@ function showPriceCoin(){
                 </div>
             `
             priceCoin.appendChild(newDiv)
-
-            
         })
-        
-        
     })
     .catch((error) => {
         console.error('Error:', error);
     });
 }
 showPriceCoin()
+
+function showBidList(){
+    const bidList = document.getElementById("bid-list")
+    const binanceApiUrl = 'https://api.binance.com/api/v3/depth';
+
+    const limit = 20;
+
+    // Construct the URL with query parameters
+    const apiUrl = `${binanceApiUrl}?symbol=${symbolCoin}&limit=${limit}`;
+   
+    // bid
+    fetch(apiUrl)
+    .then((response) => response.json())
+    .then(data=> {
+        const bids = data.bids
+        bids.forEach(value => {
+            const priceOrder = value[0]
+            const amountOrder = value[1]
+            const total = priceOrder*amountOrder;
+            newTr = document.createElement("tr")
+            newTr.innerHTML=`
+                <td style="color:rgb(241 73 63)">${Number(priceOrder).toFixed(2)}</td>
+                <td>${Number(amountOrder).toFixed(6)}</td>
+                <td>${total.toFixed(4)}</td>
+            `
+            bidList.appendChild(newTr)
+        });
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+}
+showBidList()
